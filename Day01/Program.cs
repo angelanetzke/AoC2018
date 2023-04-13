af43640a1a9f653f19ab312ea88248db2ce29aae
angelanetzke <angela.netzke@gmail.com>
2023-04-13 12:48:14 -0700

    Add day 1, part 2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,5 +1,6 @@
 ﻿var allLines = File.ReadAllLines("input.txt");
 Part1(allLines);
+Part2(allLines);
 
 void Part1(string[] allLines)
 {
@@ -16,4 +17,25 @@ void Part1(string[] allLines)
 		}
 	}
 	Console.WriteLine($"Part 1: {total}");
+}
+
+void Part2(string[] allLines)
+{
+	var usedFrequencies = new HashSet<int>();
+	int nextFrequency = 0;
+	int i = 0;
+	while (!usedFrequencies.Contains(nextFrequency))
+	{
+		usedFrequencies.Add(nextFrequency);
+		if (allLines[i][0] == '+')
+		{
+			nextFrequency += int.Parse(allLines[i][1..]);
+		}
+		else
+		{
+			nextFrequency -= int.Parse(allLines[i][1..]);
+		}
+		i = (i + 1) % allLines.Length;		
+	}
+	Console.WriteLine($"Part 2: {nextFrequency}");
 }
