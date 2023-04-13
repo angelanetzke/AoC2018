10124d01824adc2c3e6d3fcd32d984c398876714
angelanetzke <angela.netzke@gmail.com>
2023-04-13 12:43:18 -0700

    Add day 1, part 1

---
+++ Day01/Program.cs
@@ -0,0 +1,19 @@
+﻿var allLines = File.ReadAllLines("input.txt");
+Part1(allLines);
+
+void Part1(string[] allLines)
+{
+	var total = 0;
+	foreach (string thisLine in allLines)
+	{
+		if (thisLine[0] == '+')
+		{
+			total += int.Parse(thisLine[1..]);
+		}
+		else
+		{
+			total -= int.Parse(thisLine[1..]);
+		}
+	}
+	Console.WriteLine($"Part 1: {total}");
+}
