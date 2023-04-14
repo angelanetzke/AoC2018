e18ec113832ac7a6e17d3830c55a32383c1ba47f
angelanetzke <angela.netzke@gmail.com>
2023-04-13 17:49:29 -0700

    Add day 4, part 1

---
+++ Day04/Guard.cs
@@ -0,0 +1,49 @@
+namespace Day04
+{
+	internal class Guard
+	{
+		private readonly Dictionary<int, int> counts = new ();
+		private int lastEvent = 0;		
+		
+		public void NewShift()
+		{
+			lastEvent = 0;
+		}
+
+		public void AddEvent(int eventTime, bool isAwake)
+		{
+			if (isAwake)
+			{
+				for (int thisMinute = lastEvent; thisMinute < eventTime; thisMinute++)
+				{
+					AddToCount(thisMinute);
+				}
+			}
+			lastEvent = eventTime;
+		}
+
+		public void AddToCount(int minute)
+		{
+			if (counts.ContainsKey(minute))
+			{
+				counts[minute]++;
+			}
+			else
+			{
+				counts[minute] = 1;
+			}
+		}
+
+		public (int, int) GetSleepStats()
+		{
+			if (counts.Keys.Count == 0)
+			{
+				return (0, 0);
+			}
+			var totalSleepTime = counts.Values.Sum();
+			var mostCommonSleepMinute = counts.MaxBy(x => x.Value).Key;
+			return (totalSleepTime, mostCommonSleepMinute);
+		}
+
+	}
+}
---
+++ Day04/Program.cs
@@ -0,0 +1,47 @@
+﻿using Day04;
+
+var allLines = File.ReadAllLines("input.txt");
+Part1(allLines);
+
+static void Part1(string[] allLines)
+{
+	var eventList = new List<(string, int, string)>();
+	foreach (string thisLine in allLines)
+	{
+		var dateString = thisLine.Substring(1, 16);
+		var minuteValue = int.Parse(thisLine.Substring(15, 2));
+		var eventString = thisLine.Substring(19);
+		eventList.Add((dateString, minuteValue, eventString));
+	}
+	eventList = eventList.OrderBy(x => x.Item1).ToList();
+	var isAwake = true;
+	var currentGuard = new Guard();
+	var guards = new Dictionary<string, Guard>();
+	foreach ((string, int, string) thisEvent in eventList)
+	{
+		if (thisEvent.Item3.Contains("Guard"))
+		{
+			var guardID = thisEvent.Item3.Substring(7).Split(' ')[0];
+			if (!guards.ContainsKey(guardID))
+			{
+				guards[guardID] = new Guard();
+			}
+			currentGuard = guards[guardID];
+			currentGuard.NewShift();
+		}
+		else
+		{
+			isAwake = thisEvent.Item3.Contains("wakes up");
+			currentGuard.AddEvent(thisEvent.Item2, isAwake);
+		}
+	}
+	var guardData = new List<(string, int, int)>();
+	foreach (string thisGuardID in guards.Keys)
+	{
+		var thisGuardStats = guards[thisGuardID].GetSleepStats();
+		guardData.Add((thisGuardID, thisGuardStats.Item1, thisGuardStats.Item2));
+	}
+	guardData = guardData.OrderBy(x => x.Item2).ToList();
+	var result = int.Parse(guardData.Last().Item1) * guardData.Last().Item3;
+	Console.WriteLine($"Part 1: {result}");
+}
