5ee9d8ec42435215ed35f8ed3d86f267e03ad2c0
angelanetzke <angela.netzke@gmail.com>
2023-04-13 18:25:39 -0700

    Add day 5, part 1

---
+++ Day05/Program.cs
@@ -0,0 +1,26 @@
+﻿var inputLine = File.ReadAllLines("input.txt")[0];
+Part1(inputLine);
+
+static void Part1(string inputLine)
+{
+	int reactionCount;
+	var lastString = inputLine;
+	string thisString;
+	do 
+	{
+		reactionCount = 0;
+		for (char thisUnit = 'a'; thisUnit <= 'z'; thisUnit++)
+		{
+			var pair1 = thisUnit.ToString() + char.ToUpper(thisUnit);
+			var pair2 = char.ToUpper(thisUnit).ToString() + thisUnit;
+			thisString = lastString.Replace(pair1, "");
+			thisString = thisString.Replace(pair2, "");
+			if (lastString.Length > thisString.Length)
+			{
+				reactionCount++;
+			}
+			lastString = thisString;
+		}
+	} while (reactionCount > 0);
+	Console.WriteLine($"Part 1: {lastString.Length}");
+}
