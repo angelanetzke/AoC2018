741dd5808aa2d67d69aad0249d023ffe82859c27
angelanetzke <angela.netzke@gmail.com>
2023-04-14 09:29:56 -0700

    Add day 6, part 2

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,15 +1,16 @@
 ﻿using Day06;
 
 var allLines = File.ReadAllLines("input.txt");
-Part1(allLines);
-
-static void Part1(string[] allLines)
+var locationList = new List<Location>();
+foreach (string thisLine in allLines)
 {
-	var locationList = new List<Location>();
-	foreach (string thisLine in allLines)
-	{
-		locationList.Add(new Location(thisLine));
-	}
+	locationList.Add(new Location(thisLine));
+}
+Part1(locationList);
+Part2(locationList);
+
+static void Part1(List<Location> locationList)
+{	
 	var minX = locationList.Min(thisPoint => thisPoint.GetX());
 	var maxX = locationList.Max(thisPoint => thisPoint.GetX());
 	var minY = locationList.Min(thisPoint => thisPoint.GetY());
@@ -33,4 +34,24 @@ static void Part1(string[] allLines)
 	var finiteLocations = locationList.Where(x => !x.isInfinite).ToList();
 	var largestArea = finiteLocations.Max(x => x.GetPointCount());
 	Console.WriteLine($"Part 1: {largestArea}");
+}
+
+static void Part2(List<Location> locationList)
+{
+	var areaSize = 0;
+	var minX = locationList.Min(thisPoint => thisPoint.GetX());
+	var maxX = locationList.Max(thisPoint => thisPoint.GetX());
+	var minY = locationList.Min(thisPoint => thisPoint.GetY());
+	var maxY = locationList.Max(thisPoint => thisPoint.GetY());
+	for (int x = minX; x <= maxX; x++)
+	{
+		for (int y = minY; y <= maxY; y++)
+		{
+			if (locationList.Sum(thisPoint => thisPoint.GetDistanceToPoint(x, y)) < 10_000)
+			{
+				areaSize++;
+			}
+		}
+	}
+	Console.WriteLine($"Part 2: {areaSize}");
 }
