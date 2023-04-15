d1f4e5669bf0c305e8ff123a2a2ddc4be777bce4
angelanetzke <angela.netzke@gmail.com>
2023-04-14 19:52:56 -0700

    Add day 9, part 2

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -2,12 +2,27 @@
 
 var inputLine = File.ReadAllLines("input.txt")[0];
 Part1(inputLine);
+Part2(inputLine);
 
 static void Part1(string inputLine)
 {
 	var playerCount = int.Parse(inputLine.Split(' ')[0]);
 	var lastMarble = int.Parse(inputLine.Split(' ')[6]);
-	var playerScores = Enumerable.Repeat(0, playerCount).ToArray();
+	var highScore = PlayGame(lastMarble, playerCount);
+	Console.WriteLine($"Part 1: {highScore}");
+}
+
+static void Part2(string inputLine)
+{
+	var playerCount = int.Parse(inputLine.Split(' ')[0]);
+	var lastMarble = int.Parse(inputLine.Split(' ')[6]) * 100;
+	var highScore = PlayGame(lastMarble, playerCount);
+	Console.WriteLine($"Part 2: {highScore}");
+}
+
+static long PlayGame(int lastMarble, int playerCount)
+{
+	var playerScores = Enumerable.Repeat(0L, playerCount).ToArray();
 	var theCircle = new MarbleCirle();
 	for (int i = 1; i <= lastMarble; i++)
 	{
@@ -15,5 +30,5 @@ static void Part1(string inputLine)
 		playerScores[i % playerCount] += nextScore;
 	}
 	var highScore = playerScores.Max();
-	Console.WriteLine($"Part 1: {highScore}");
+	return highScore;
 }
