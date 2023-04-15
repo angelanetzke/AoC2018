2ce3eb893ce9d6655747aaad2d665ba2c0ada497
angelanetzke <angela.netzke@gmail.com>
2023-04-14 18:09:51 -0700

    Add day 8, part 1

---
+++ Day08/Node.cs
@@ -0,0 +1,52 @@
+namespace Day08
+{
+	internal class Node
+	{
+		private static int[] values = Array.Empty<int>();
+		private int metadataStart;
+		private int metadataCount;
+		private List<Node> children = new ();
+
+		public static void SetValues(string inputLine)
+		{
+			values = inputLine.Split(' ').Select(x => int.Parse(x)).ToArray();
+		}
+
+		public Node() : this(0)
+		{}
+		public Node(int start)
+		{
+			var childrenCount = values[start];
+			metadataCount = values[start + 1];
+			if (childrenCount == 0)
+			{
+				metadataStart = start + 2;
+			}
+			else
+			{
+				var nextStart = start + 2;
+				for (int i = 1; i <= childrenCount; i++)
+				{
+					children.Add(new Node(nextStart));
+					nextStart = children.Last().metadataStart + children.Last().metadataCount;
+				}
+				metadataStart = nextStart;		
+			}
+		}
+
+		public int GetMetadataSum()
+		{
+			int sum = 0;
+			for (int i = metadataStart; i < metadataStart + metadataCount; i++)
+			{
+				sum += values[i];
+			}
+			foreach (Node thisChild in children)
+			{
+				sum += thisChild.GetMetadataSum();
+			}
+			return sum;
+		}
+
+	}
+}
---
+++ Day08/Program.cs
@@ -0,0 +1,13 @@
+﻿using Day08;
+
+var inputLine = File.ReadAllLines("input.txt")[0];
+Part1(inputLine);
+
+static void Part1(string inputLine)
+{
+	Node.SetValues(inputLine);
+	var root = new Node();
+	var sum = root.GetMetadataSum();
+	Console.WriteLine($"Part 1: {sum}");
+}
+
